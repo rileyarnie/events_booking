396bf944f70c104fdabd9578703ee654ea34d1a5
rileyarnie <arnold.ril3y@gmail.com>
2024-09-30 10:32:21 +0300

    create users table and link it to events table with foreign key

--- routes/routes.go
+++ routes/routes.go
@@ -3,9 +3,12 @@ package routes
 import "github.com/gin-gonic/gin"
 
 func RegisterRoutes(server *gin.Engine) {
+	//event routes
 	server.GET("/events", getEvents)
 	server.GET("/events/:id", getEvent)
 	server.POST("/events", createEvent)
 	server.PUT("/events/:id", updateEvent)
 	server.DELETE("/events/:id", deleteEvent)
+	//user routes
+	server.POST("/signup")
 }
